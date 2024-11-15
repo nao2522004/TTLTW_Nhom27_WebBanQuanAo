cccb8406beb7ad9315615b785135745401e78157
nao2522004 <22130163@st.hcmuaf.edu.vn>
2024-11-15 22:36:58 +0700

    update hehehe

---
+++ assets/js/base.js
@@ -0,0 +1,8 @@
+// Js xu ly su kien menu cua header khi khi nho man hinh
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(' .menu-toggle').addEventListener('click', function () {
+      document.querySelector('header .part2 ul').classList.toggle('active');
+    });
+  });
+
+  // end Js xu ly su kien menu cua header khi khi nho man hinh
