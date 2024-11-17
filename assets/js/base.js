c7cec310ae0910846bff61e7954cc3d8664620ed
nao2522004 <22130163@st.hcmuaf.edu.vn>
2024-11-17 13:40:16 +0700

    Complete basic mobile web page

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,8 +1,10 @@
 // Js xu ly su kien menu cua header khi khi nho man hinh
 document.addEventListener("DOMContentLoaded", function () {
+  // Đảm bảo rằng menu không có lớp active khi trang được load lần đầu
+  document.querySelector('header .part2 ul').classList.remove('active');
+
     document.querySelector(' .menu-toggle').addEventListener('click', function () {
       document.querySelector('header .part2 ul').classList.toggle('active');
     });
   });
-
   // end Js xu ly su kien menu cua header khi khi nho man hinh
