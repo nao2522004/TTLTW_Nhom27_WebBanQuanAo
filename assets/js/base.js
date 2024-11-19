3a8667d27ee23fb01e3308865df60c083ad67c0f
nao2522004 <22130163@st.hcmuaf.edu.vn>
2024-11-19 23:50:55 +0700

    update page

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,19 +1,51 @@
 // Js xu ly su kien menu cua header khi khi nho man hinh
 document.addEventListener("DOMContentLoaded", function () {
-  const menu = document.querySelector("header .part2 ul");
 
-  if (menu) {
-    // Kiểm tra menu có tồn tại hay không
-    menu.classList.remove("active");
+  const menu = document.querySelector("header .container-nav");
+  const menu1 = document.querySelector("header .container-nav nav");
 
-    document
-      .querySelector(".menu-toggle")
-      .addEventListener("click", function () {
-        menu.classList.toggle("active");
-      });
-  }
+    if (menu && menu1) {
+        // Kiểm tra menu có tồn tại hay không
+        menu.classList.remove("active");
+        menu1.classList.remove("active");
+
+        document
+            .querySelector(".menu-toggle")
+            .addEventListener("click", function () {
+                menu.classList.toggle("active");
+                menu1.classList.toggle("active");
+            });
+    }
+
 });
 // end Js xu ly su kien menu cua header khi khi nho man hinh
+
+// Hiệu ứng chuyển động cho Header khi người dùng cuộn trang
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth > 1456) {
+      if (window.scrollY > 50) {
+          header.classList.add("scrolled");
+      } else {
+          header.classList.remove("scrolled");
+      }
+  } else {
+      header.classList.remove("scrolled"); // Loại bỏ class nếu màn hình nhỏ hơn 991px
+  }
+});
+
+// Kiểm tra và cập nhật ngay khi resize màn hình
+window.addEventListener("resize", () => {
+  const header = document.querySelector("header");
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth <= 1456) {
+      header.classList.remove("scrolled"); // Đảm bảo không áp dụng hiệu ứng cho màn hình nhỏ
+  }
+});
+// end
 
 // xu ly chuyen doi atr thanh vnd
 document.addEventListener("DOMContentLoaded", () => {
