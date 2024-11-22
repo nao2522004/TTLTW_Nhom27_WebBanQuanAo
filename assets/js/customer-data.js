f6f8bac42356057f684cb3e660cb7623319125b1
nao2522004 <22130163@st.hcmuaf.edu.vn>
2024-11-22 23:02:09 +0700

    add admin page version 2

---
+++ assets/js/customer-data.js
@@ -0,0 +1,10 @@
+const CUSTOMER_DATA = [
+    {
+        cusId: "C01",
+        cusName: "HoangLeNguyenManh",
+        cusPhone: "039000100",
+        cusAddress: "DakNong",
+        cusEmail: "Admin@gmail,com",
+        cusPermission: "Admin"
+    },
+]
