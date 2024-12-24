a8be832d22e157aaad8e5940be3efdb5d2920321
HQLuat <22130157@st.hcmuaf.edu.vn>
2024-12-24 17:15:24 +0700

    Hien thi danh sach

---
+++ src/main/java/vn/edu/hcmuaf/fit/webbanquanao/controller/ListProduct.java
@@ -0,0 +1,29 @@
+package vn.edu.hcmuaf.fit.webbanquanao.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.webbanquanao.dao.model.Product;
+import vn.edu.hcmuaf.fit.webbanquanao.service.ProductService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "ListProduct", value = "/ListProduct")
+public class ListProduct extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Product> list = new ArrayList<Product>();
+        ProductService service = new ProductService();
+        list = service.getListOfProducts();
+
+        request.setAttribute("products", list);
+        request.getRequestDispatcher("men.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
