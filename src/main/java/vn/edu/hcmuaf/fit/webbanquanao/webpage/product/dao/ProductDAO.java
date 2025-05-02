ab6c2dbd6a103775c1f27129257af4aa259c4abe
HQLuat <22130157@st.hcmuaf.edu.vn>
2025-05-02 15:33:20 +0700

    fix productDAO completely

--- src/main/java/vn/edu/hcmuaf/fit/webbanquanao/webpage/product/dao/ProductDAO.java
+++ src/main/java/vn/edu/hcmuaf/fit/webbanquanao/webpage/product/dao/ProductDAO.java
@@ -11,24 +11,16 @@ import java.sql.SQLException;
 import java.util.*;
 
 public class ProductDAO {
-    List<Product> data;
     JDBIConnector conn;
     String query, query2;
 
     public ProductDAO() {
         conn = new JDBIConnector();
-        data = getAllProducts();
     }
 
     // Get product by id
     public Product getById(int id) {
-        List<Product> products = getAllProducts();
-        for (Product product : products) {
-            if (product.getId() == id) {
-                return product;
-            }
-        }
-        return null;
+        return getAllProducts().stream().filter(p -> p.getId() == id).findFirst().orElse(null);
     }
 
     // Get all products
@@ -153,13 +145,42 @@ public class ProductDAO {
         return getProductsByQuery(query, query2, category);
     }
 
+    // search products by name
+    public List<Product> searchByName(String name) {
+        query = "SELECT" +
+                "   id," +
+                "   typeId," +
+                "   categoryId," +
+                "   supplierId," +
+                "   productname," +
+                "   description," +
+                "   releaseDate," +
+                "   unitSold," +
+                "   unitPrice," +
+                "   status," +
+                "   id AS parameter " +
+                "FROM products " +
+                "WHERE productname LIKE ?";
+        query2 = "SELECT" +
+                "   id," +
+                "   productId," +
+                "   size," +
+                "   stock," +
+                "   image," +
+                "   color " +
+                "FROM product_details " +
+                "WHERE productId = ?";
+
+        return getProductsByQuery(query, query2, name);
+    }
+
     // Get products by query
-    public List<Product> getProductsByQuery(String sql1, String sql2, @Nullable String category) {
+    public List<Product> getProductsByQuery(String sql1, String sql2, @Nullable String parameter) {
         List<Product> list = new ArrayList<>();
         return conn.get().withHandle(h -> {
             try (PreparedStatement stmt = h.getConnection().prepareStatement(sql1)) {
-                if (category != null) {
-                    stmt.setString(1, category);
+                if (parameter != null && !parameter.isEmpty()) {
+                    stmt.setString(1, parameter);
                 }
                 try (ResultSet rs = stmt.executeQuery()) {
                     while(rs.next()) {
@@ -317,77 +338,10 @@ public class ProductDAO {
         return re;
     }
 
-    // search products by name
-    public Map<Integer, Product> searchByName(String name) {
-        query = "SELECT" +
-                "   p.id AS id," +
-                "   t.name AS type," +
-                "   c.name AS category," +
-                "   s.supplierName AS supplier," +
-                "   p.productname AS name," +
-                "   p.description AS description," +
-                "   p.releaseDate AS releaseDate," +
-                "   p.unitSold AS unitSold," +
-                "   p.unitPrice AS unitPrice," +
-                "   p.status AS status," +
-                "   pd.size AS size," +
-                "   pd.stock AS stock," +
-                "   pd.image AS image," +
-                "   pd.color AS color, " +
-                "   pd.id AS productDetailId " +
-                "FROM products p " +
-                "JOIN product_details pd ON p.id = pd.productId " +
-                "JOIN categories c ON p.categoryId = c.id " +
-                "JOIN types t ON p.typeId = t.id " +
-                "JOIN suppliers s ON p.supplierId = s.id " +
-                "WHERE p.productname LIKE ?";
-
-//        return conn.get().withHandle(h -> {
-//            Map<Integer, Product> result = new HashMap<>();
-//            try (PreparedStatement stmt = h.getConnection().prepareStatement(query)) {
-//                stmt.setString(1, "%" + name + "%");
-//                try (ResultSet rs = stmt.executeQuery()) {
-//                    while (rs.next()) {
-//                        int productId = rs.getInt("id");
-//                        Product p = result.getOrDefault(productId, new Product());
-//
-//                        p.setId(productId);
-//                        p.setType(rs.getString("type"));
-//                        p.setCategory(rs.getString("category"));
-//                        p.setSupplier(rs.getString("supplier"));
-//                        p.setName(rs.getString("name"));
-//                        p.setDescription(rs.getString("description"));
-//                        p.setReleaseDate(rs.getDate("releaseDate"));
-//                        p.setUnitSold(rs.getInt("unitSold"));
-//                        p.setUnitPrice(rs.getDouble("unitPrice"));
-//                        p.setStatus(rs.getBoolean("status"));
-//                        p.setStock(rs.getInt("stock"));
-//
-//                        String size = rs.getString("size");
-//                        String image = rs.getString("image");
-//                        String color = rs.getString("color");
-//
-//                        if (!p.getSizes().contains(size))
-//                            p.getSizes().add(size);
-//                        if (!p.getImages().contains(image))
-//                            p.getImages().add(image);
-//                        if (!p.getColors().contains(color))
-//                            p.getColors().add(color);
-//
-//                        result.put(productId, p);
-//                    }
-//                }
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//            return result;
-//        });
-        Map<Integer, Product> result = new HashMap<>();
-        return result;
-    }
-
     public static void main(String[] args) {
         ProductDAO dao = new ProductDAO();
-        System.out.println(dao.getFilteredProducts("Nam", new String[]{"Quần"}, new String[]{"M"}, null, null));
+//        System.out.println(dao.getFilteredProducts("Nam", new String[]{"Quần"}, new String[]{"M"}, null, null));
+//        System.out.println(dao.searchByName("Quần tây PEALO form cạp cao có xếp li"));
+        System.out.println(dao.getById(12));
     }
 }
