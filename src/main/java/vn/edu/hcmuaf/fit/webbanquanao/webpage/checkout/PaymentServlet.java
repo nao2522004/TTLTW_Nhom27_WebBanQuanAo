9a2170e3f45f6ef64cabbda5d946a06402999f1b
HQLuat <22130157@st.hcmuaf.edu.vn>
2025-05-20 22:59:09 +0700

    Config

--- src/main/java/vn/edu/hcmuaf/fit/webbanquanao/webpage/checkout/PaymentServlet.java
+++ src/main/java/vn/edu/hcmuaf/fit/webbanquanao/webpage/checkout/PaymentServlet.java
@@ -28,23 +28,23 @@ public class PaymentServlet extends HttpServlet {
         long amount = Integer.parseInt(request.getParameter("amount"))*100;
         String bankCode = request.getParameter("bankCode");
 
-//        String vnp_TxnRef = Config.getRandomNumber(8);
-//        String vnp_IpAddr = Config.getIpAddress(request);
-//
-//        String vnp_TmnCode = Config.vnp_TmnCode;
+        String vnp_TxnRef = Config.getRandomNumber(8);
+        String vnp_IpAddr = Config.getIpAddress(request);
+
+        String vnp_TmnCode = Config.vnp_TmnCode;
 
         Map<String, String> vnp_Params = new HashMap<>();
         vnp_Params.put("vnp_Version", vnp_Version);
         vnp_Params.put("vnp_Command", vnp_Command);
-//        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
         vnp_Params.put("vnp_Amount", String.valueOf(amount));
         vnp_Params.put("vnp_CurrCode", "VND");
 
         if (bankCode != null && !bankCode.isEmpty()) {
             vnp_Params.put("vnp_BankCode", bankCode);
         }
-//        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
-//        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
         vnp_Params.put("vnp_OrderType", orderType);
 
         String locate = request.getParameter("language");
@@ -53,8 +53,8 @@ public class PaymentServlet extends HttpServlet {
         } else {
             vnp_Params.put("vnp_Locale", "vn");
         }
-//        vnp_Params.put("vnp_ReturnUrl", Config.vnp_ReturnUrl);
-//        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+        vnp_Params.put("vnp_ReturnUrl", Config.vnp_ReturnUrl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
 
         Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
@@ -89,13 +89,13 @@ public class PaymentServlet extends HttpServlet {
             }
         }
         String queryUrl = query.toString();
-//        String vnp_SecureHash = Config.hmacSHA512(Config.secretKey, hashData.toString());
-//        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
-//        String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
+        String vnp_SecureHash = Config.hmacSHA512(Config.secretKey, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
         com.google.gson.JsonObject job = new JsonObject();
         job.addProperty("code", "00");
         job.addProperty("message", "success");
-//        job.addProperty("data", paymentUrl);
+        job.addProperty("data", paymentUrl);
         Gson gson = new Gson();
         response.getWriter().write(gson.toJson(job));
     }
